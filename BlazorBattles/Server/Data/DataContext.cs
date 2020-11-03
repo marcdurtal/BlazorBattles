cd93b9fec3f6e8e27f6b82d918e3e3a53bbb0ab3
Philippe Germain-Bonne <PhilippeGermainBonne@hotmail.com>
2020-11-03 12:58:15 +0100

    Implement the DataContext

---
+++ BlazorBattles/Server/Data/DataContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BlazorBattles.Shared;
+using Microsoft.EntityFrameworkCore;
+
+namespace BlazorBattles.Server.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Unit> Units { get; set; }
+    }
+}
