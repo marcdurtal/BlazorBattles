0060a1523d70f9d09923988d606a3232aa2272ef
Philippe Germain-Bonne <PhilippeGermainBonne@hotmail.com>
2020-11-03 16:31:46 +0100

    Authentication Repository

---
+++ BlazorBattles/Server/Data/IAuthRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using BlazorBattles.Shared;
+using System.Threading.Tasks;
+
+namespace BlazorBattles.Server.Data
+{
+    public interface IAuthRepository
+    {
+        Task<int> Register(User user, string password);
+        Task<string> Login(string email, string password);
+        Task<bool> UserExists(string email);
+    }
+}
