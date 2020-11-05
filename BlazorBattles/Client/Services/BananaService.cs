642c22068138f9eda1f4806439683af472ac7e79
Philippe Germain-Bonne <PhilippeGermainBonne@hotmail.com>
2020-11-05 15:31:30 +0100

    Add Bananas to the Database

--- BlazorBattles/Client/Services/BananaService.cs
+++ BlazorBattles/Client/Services/BananaService.cs
@@ -29,11 +29,17 @@ namespace BlazorBattles.Client.Services
 
         void BananasChanged() => OnChange.Invoke();
 
-        public void AddBananas(int amount)
+
+
+        public async Task AddBananas(int amount)
         {
-            Bananas += amount;
+
+            var result = await _http.PutAsJsonAsync<int>("api/User/AddBananas", amount);
+            Bananas = await result.Content.ReadFromJsonAsync<int>();
             BananasChanged();
         }
+
+
 
         public async Task GetBananas()
         {
